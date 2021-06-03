c32b6660cb60b3f4b7d5bc8aef8791a55e939be5
unknown <fjeferson60@gmail.com>
2021-06-03 07:46:57 -0300

    Estudando Switch Case JavaScript

---
+++ aula17.js
@@ -0,0 +1,15 @@
+//AUTOR: Jeferson
+//DESCRIÇÃO: Estrutura condicional switch
+
+var sexo = 'F';
+switch(sexo) {
+    case 'M':
+        alert('Macho');
+        break;
+
+        case 'F':
+        alert('Femea');
+        break;
+        default:
+            alert('Indefinido')
+}
---
+++ exemplo2.js
@@ -0,0 +1,21 @@
+
+//DECRARAÇÃO E ESTRUTURAS 
+var tecnologia = 'java';
+switch (tecnologia) {
+    case 'java':
+    case 'c++':
+    case 'javascript':
+    case 'c#':
+        alert('Linguagem de programação');
+        break;
+
+    case 'sqlserver':
+    case 'postgresql':
+    case 'oracle':
+        alert('Banco de dados');
+        break;
+    default:
+        alert('Tecnologia desconhecida');
+}
+
+//INDENTAÇÃO ALT + SHIFT + F
